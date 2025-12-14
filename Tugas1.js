930c93272236c7e3c36f68a52cd2a2dce511689b
hanifahharnanda-afk <hanifahharnanda@gmail.com>
2025-12-14 08:54:37 +0700

    feat: age logic

--- Tugas1.js
+++ Tugas1.js
@@ -1,1 +1,10 @@
 const input = require('prompt-sync')({sigint: true});
+
+const Nama = input("Masukkan nama: ");
+const Umur = Number(input("Masukkan umur: "));
+
+if (Umur < 21) {
+    console.log(`Maaf ${Nama}, umur kamu kurang ${21 - Umur} tahun lagi`);
+} else {
+   const Uang = Number(input("Masukkan jumlah uang: "));
+}
