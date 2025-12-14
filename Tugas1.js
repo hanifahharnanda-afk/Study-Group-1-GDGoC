eead77f6fa487425d736cb2907b190fcdb054265
hanifahharnanda-afk <hanifahharnanda@gmail.com>
2025-12-14 08:45:26 +0700

    feat: money logic

--- Tugas1.js
+++ Tugas1.js
@@ -7,4 +7,10 @@ if (Umur < 21) {
     console.log(`Maaf ${Nama}, umur kamu kurang ${21 - Umur} tahun lagi`);
 } else {
    const Uang = Number(input("Masukkan jumlah uang: "));
+
+    if (Uang < 500000) {
+        console.log(`Maaf ${Nama}, uang kamu cuma ${Uang} ribu, datang lagi lain kali`);
+    } else {
+        console.log(`Selamat datang ${Nama}`);
+    }
 }
