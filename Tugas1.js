b51ba7fe9f32e80c6053c9ea0a84228eae19b2c0
hanifahharnanda-afk <hanifahharnanda@gmail.com>
2025-12-14 09:42:29 +0700

    chore: init project

--- Tugas1.js
+++ Tugas1.js
@@ -1,16 +1,1 @@
 const input = require('prompt-sync')({sigint: true});
-
-const Nama = input("Masukkan nama: ");
-const Umur = Number(input("Masukkan umur: "));
-
-if (Umur < 21) {
-    console.log(`Maaf ${Nama}, umur kamu kurang ${21 - Umur} tahun lagi`);
-} else {
-   const Uang = Number(input("Masukkan jumlah uang: "));
-
-    if (Uang < 500000) {
-        console.log(`Maaf ${Nama}, uang kamu cuma ${Uang} ribu, datang lagi lain kali`);
-    } else {
-        console.log(`Selamat datang ${Nama}`);
-    }
-}
